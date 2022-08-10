4a919261f15e14074f843e0e826adbe1cae0b3ae
Alexshan20 <shangina.1998@gmail.com>
2022-08-10 10:14:05 +0300

    pagination, context, search

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 
-export default function Categories() {
-  const [activeIndex, setActiveIndex] = React.useState(0);
-
-  const categories = ['Все', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+export default function Categories({ value, onChangeCategory }) {
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 
   return (
     <div className="categories">
       <ul>
         {categories.map((categoryName, i) => (
-          <li
-            key={i}
-            onClick={() => setActiveIndex(i)}
-            className={activeIndex === i ? 'active' : ''}>
+          <li key={i} onClick={() => onChangeCategory(i)} className={value === i ? 'active' : ''}>
             {categoryName}
           </li>
         ))}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,26 +4,45 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Sceleton from '../components/PizzaBlock/Sceleton';
+import { SearchContext } from '../App';
+import { Pagination } from '../components/Pagination';
 
 export default function Home() {
+  const { searchValue } = React.useContext(SearchContext);
+
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [sortType, setSortType] = React.useState({
+    name: 'популярности (DESC)',
+    sortProperty: 'rating',
+  });
 
   React.useEffect(() => {
-    fetch('https://62ece332818ab252b604bcff.mockapi.io/items')
+    setIsLoading(true);
+
+    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+    const sortBy = sortType.sortProperty.replace('-', '');
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
+
+    fetch(
+      `https://62ece332818ab252b604bcff.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+    )
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType, searchValue, currentPage]);
 
   return (
     <>
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)} />
+        <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
 
@@ -32,6 +51,7 @@ export default function Home() {
           ? [...new Array(6)].map((_, index) => <Sceleton key={index} />)
           : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </>
   );
 }
